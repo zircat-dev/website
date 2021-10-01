87ac39c95b2228e90951d942b8332529c733f8bb
Nicholas Kircher <MiracleBlue@users.noreply.github.com>
2021-10-02 08:17:48 +1000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-
+import { values } from 'ramda';
 import { Container, Row, Col } from 'react-grid-system';
 
-import {
-  SrOnly
-} from '../common/utils/style';
+import { SrOnly } from '../common/utils/style';
 import Logo from '../common/logo';
 
-const baseUrl = '.';
-
-const Header = styled.div`
+const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 99vh;
+`;
+
+const CenterImage = styled.div`
   .image {
-    position: fixed;
+    position: absolute;
     left: 50%;
     top: 50%;
     width: 200px;
@@ -24,42 +24,181 @@ const Header = styled.div`
   }
 `;
 
-const Image = styled.div({
-  width: '100%',
-  height: '100px',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'object-fit',
-  dislpay: 'flex',
-  marginLeft: '4px',
-  '&::first-child': {
-    marginLeft: 0
+const listStyles = css`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const List = styled.ul`
+  ${listStyles};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+const ListItem = styled.li`
+  ${listStyles};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-sizing: border-box;
+  justify-content: center;
+  padding: 10px;
+  border-left: 1px solid grey;
+  &:last-child {
+    border: none;
   }
-}, (
-  (props => props.imageSrc && {
-    backgroundImage: `url(${props.imageSrc})`
-  })
-))
+  ${props =>
+    props.isActive &&
+    css`
+      background: red;
+    `}
+`;
+
+const NavigationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-end;
+  width: 100%;
+  height: 72px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: white;
+`;
+
+const Navigation = ({ options = {} }) => {
+  const [selectedId, setSelectedId] = useState(-1);
+  const onSelect = item => {
+    setSelectedId(item.id);
+  };
+
+  const renderNav = () => {
+    const navItems = values(options);
+    if (!navItems || !navItems.length) return null;
+    return (
+      <List>
+        {navItems.map(item => (
+          <ListItem
+            key={item.id}
+            isActive={selectedId === item.id}
+            onClick={() => onSelect(item.id)}
+          >
+            {item.label}
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
+  return <NavigationContainer>{renderNav()}</NavigationContainer>;
+};
+
+const Header = ({ children }) => {
+  return <HeaderContainer>{children}</HeaderContainer>;
+};
 
 const LogoContainer = styled.div({
   width: '400px',
-  // height: '100px',
 });
 
+const Content = styled.div`
+  width: 100%;
+  min-height: 300px;
+  background: green;
+  position: relative;
+`;
+
+const HeaderLogo = styled.div`
+  width: 100px;
+`;
+
 const TopHeader = styled.h1(SrOnly);
+
+const Something = styled.div`
+  background: red;
+  width: 100%;
+  height: 100px;
+`;
 
 const Landing = () => {
   return (
     <Container>
+      <Something>Something</Something>
       <Header>
-        <TopHeader>Zircat: The Web Development Consultancy of your Dreams</TopHeader>
-        <LogoContainer>
+        <HeaderLogo>
           <Logo />
-        </LogoContainer>
+        </HeaderLogo>
+        <Navigation
+          options={{
+            about: { id: 'about', route: '/', label: 'About' },
+            team: { id: 'team', route: '/', label: 'Team' },
+            contact: { id: 'contact', route: '/', label: 'Contact' },
+          }}
+        />
       </Header>
+      <TopHeader>
+        Zircat: The Web Development Consultancy of your Dreams
+      </TopHeader>
+      <Content id="scroll-1">
+        <CenterImage>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+        </CenterImage>
+      </Content>
+      <Content id="scroll-2">Some content</Content>
       <Row>
         <Col>
           <h2>Who are we, really?</h2>
-          <p>We are amazing.  That's who.  Yeah.</p>
+          <h2>Looser</h2>
+          <p>We are amazing. That's who. Yeah.</p>
+          {/*Main pitch*/}
+          <p>
+            With over a decade of experience working on dozens of projects at
+            every imaginable size and budget, we've learned a few things along
+            the way, and we love being able to share that unique expertise. From
+            enterprises like Vodafone, Origin and Bupa, to agencies like Next
+            Digital and DT, to various start-ups, and even the NSW Department of
+            Justice, our specialists have made careers with proven track records
+            of success at every scale. They've done rapid-feedback prototyping
+            in tandem with UI and UX designers; built campaigns and highly
+            animated websites for brands like Honda, Ford and Bonds; built large
+            and sophisticated web applications; built custom tools, libraries
+            and frameworks to make developers lives easier; managed teams of
+            developers large and small, built out development teams large and
+            small, mentored and upskilled developers at all levels, and even
+            personally lead entire digital transformations of enterprise
+            organisations; and in everything we do, we pursue with determination
+            the very best possible outcomes.
+          </p>
+          {/*Special pitch */}
+          <p>
+            So you need an in-house software team, but you don't know where to
+            start, who to hire, or what it needs to be set up for success.
+            You've thought about bringing in a big-name consultancy, but they
+            only "loan" people to you for a short time. You need your own
+            people, your own team, your own expertise, under your own
+            organisation. That's where we come in. Zircat specialises in
+            building out world-class software teams for organisations of all
+            kinds. We can work with you to determine what roles to hire, what
+            structures and processes should be put in place, and develop a
+            step-by-step roadmap showing the way. What about implementing that
+            roadmap? Luckily, you can leave all that to us! We'll take care of
+            hiring the right people (under your own organisation), training and
+            upskilling on engineering best practices, and embedding the most
+            appropriate agile methodology suited to the needs of your business.
+            We will then give you regular progress updates so that you're always
+            in the loop, and you will have oversight over the entire journey.
+            Wanna know the best bit? When we're all done, you get to keep
+            everything! Your team, your expertise, your personnel are all part
+            of your organisation. Only our small team of Zircat specialists will
+            move on, leaving you with a world-class engineering team all of your
+            own, customised and designed around your business needs.
+          </p>
         </Col>
       </Row>
     </Container>
