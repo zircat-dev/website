bae15ecac5b4ffa5201cc023885a5d571cb85eb8
the-captain-america <philip.ronald.hultgren@gmail.com>
2021-10-02 17:02:59 +1000

    add footer

--- features/Landing/Controller/Controller.js
+++ features/Landing/Controller/Controller.js
@@ -2,11 +2,14 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import { Header } from '../Header';
+import { Footer } from '../Footer';
 import { StickyHeader } from './StickyHeader';
 
 const ScrollCatch = styled.div`
   max-width: 960px;
   margin: 0 auto;
+  padding-left: 24px;
+  padding-right: 24px;
 `;
 
 const Controller = ({ children }) => {
@@ -21,6 +24,7 @@ const Controller = ({ children }) => {
         <Header />
         {children}
       </ScrollCatch>
+      <Footer />
     </>
   );
 };
